38758ba81d48ab4e9a6ea0a0b0897951ef472c4c
jtsato <jorge.takeshi.sato@gmail.com>
2023-09-23 16:36:19 -0300

    feat(wip): work in progress

--- run-local.cmd
+++ run-local.cmd
@@ -1,4 +1,8 @@
+@echo off
 terraform destroy -auto-approve -var-file=secrets.tfvars
+
 terraform refresh -var-file=secrets.tfvars
+
 terraform plan -out=tfplan.out -var-file=secrets.tfvars
+
 terraform apply -auto-approve tfplan.out
